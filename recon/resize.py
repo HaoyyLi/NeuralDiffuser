853955087b2b327d61ba8e03385be60d7a68fc65
H.Y Li <1160426545@qq.com>
2024-01-27 22:40:39 +0800

    1

--- recon/resize.py
+++ recon/resize.py
@@ -3,8 +3,8 @@ import os
 import numpy as np 
 from PIL import Image
 # %%
-dirpath='G:/workware/src/fMRI/Decoding/StructureConstiantMindDiffusion/output/tv01-guided-sag/subj01/0.0'
-savepath = 'E:/GitHub/NeuralDiffuser/recon/0.0/'
+dirpath='G:/workware/src/fMRI/Decoding/StructureConstiantMindDiffusion/output/tv01-guided-sag-us/subj01/500000.0/0.2'
+savepath = 'E:/GitHub/NeuralDiffuser/recon/500000.0/0.2/'
 os.makedirs(savepath, exist_ok=True)
 img_path = sorted([os.path.join(dirpath, name) for name in os.listdir(dirpath) if name.endswith('.png')])
 # %%
